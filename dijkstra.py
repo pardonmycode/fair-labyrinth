552cd5512d80bf53199c54b0f26f5afcdcbfa371
PirateCat <106189370+pardonmycode@users.noreply.github.com>
2023-04-14 18:42:14 +0200

    def solve maze

--- dijkstra.py
+++ dijkstra.py
@@ -4,3 +4,9 @@ from priority_queue import FibPQ, HeapPQ, QueuePQ
 def solve(maze):
     # Width is used for indexing, total is used for array sizes
     width = maze.width
+from FibonacciHeap import FibHeap
+from priority_queue import FibPQ, HeapPQ, QueuePQ
+
+def solve(maze):
+    # Width is used for indexing, total is used for array sizes
+    width = maze.width
