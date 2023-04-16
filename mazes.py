24ae53db20a0ef2bcba820212054c55a926d2f86
PirateCat <106189370+pardonmycode@users.noreply.github.com>
2023-04-16 10:41:32 +0200

    def solve maze

---
+++ mazes.py
@@ -0,0 +1,6 @@
+class Maze:
+    class Node:
+        def __init__(self, position):
+            self.Position = position
+            self.Neighbours = [None, None, None, None]
+            #self.Weights = [0, 0, 0, 0]
