c73535dee0c831d0c86246bf1b5eb475b0ede704
PirateCat <106189370+pardonmycode@users.noreply.github.com>
2023-05-16 12:32:27 +0200

    end rer.sear

--- depthfirst.py
+++ depthfirst.py
@@ -38,3 +38,29 @@ def solve(maze):
         current = prev[current.Position[0] * width + current.Position[1]]
 
     return [path, [count, len(path), completed]]
+from collections import deque
+
+def solve(maze):
+    start = maze.start
+    end = maze.end
+    width = maze.width
+    stack = deque([start])
+    shape = (maze.height, maze.width)
+    prev = [None] * (maze.width * maze.height)
+    visited = [False] * (maze.width * maze.height)
+    count = 0
+
+    completed = False
+    while stack:
+        count += 1
+        current = stack.pop()
+
+        if current == end:
+            completed = True
+            break
+
+        visited[current.Position[0] * width + current.Position[1]] = True
+
+        #import code
+        #code.interact(local=locals())
+
