76277d218b46b2f3d87aec3ba9acd0c05df7a3bc
PirateCat <106189370+pardonmycode@users.noreply.github.com>
2023-06-18 18:42:46 +0200

    fix bugs

--- astar.py
+++ astar.py
@@ -101,3 +101,29 @@ def solve(maze):
         current = prev[current.Position[0] * width + current.Position[1]]
 
     return [path, [count, len(path), completed]]
+from FibonacciHeap import FibHeap
+from priority_queue import FibPQ, HeapPQ, QueuePQ
+
+# This implementatoin of A* is almost identical to the Dijkstra implementation. So for clarity I've removed all comments, and only added those
+# Specifically showing the difference between dijkstra and A*
+
+def solve(maze):
+    width = maze.width
+    total = maze.width * maze.height
+
+    start = maze.start
+    startpos = start.Position
+    end = maze.end
+    endpos = end.Position
+
+    visited = [False] * total
+    prev = [None] * total
+
+    infinity = float("inf")
+    distances = [infinity] * total
+
+    # The priority queue. There are multiple implementations in priority_queue.py
+    # unvisited = FibHeap()
+    unvisited = HeapPQ()
+    # unvisited = FibPQ()
+    # unvisited = QueuePQ()
