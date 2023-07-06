87d4cc54d175fea60c97de8390691a5ab34b1b80
PirateCat <106189370+pardonmycode@users.noreply.github.com>
2023-07-06 11:43:16 +0200

    more dynamic

--- priority_queue.py
+++ priority_queue.py
@@ -128,3 +128,29 @@ class QueuePQ(PriorityQueue):
         node.key = new_priority
         self.insert(node)
 
+
+from abc import ABCMeta, abstractmethod
+import itertools
+
+from FibonacciHeap import FibHeap
+import heapq
+import Queue
+
+class PriorityQueue():
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def __len__(self): pass
+
+    @abstractmethod
+    def insert(self, node): pass
+
+    @abstractmethod
+    def minimum(self): pass
+
+    @abstractmethod
+    def removeminimum(self): pass
+
+    @abstractmethod
+    def decreasekey(self, node, new_priority): pass
+
