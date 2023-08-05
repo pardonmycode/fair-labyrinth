48b4da8177fb17147d7eae29f89cde6e866341e2
PirateCat <106189370+pardonmycode@users.noreply.github.com>
2023-08-05 09:43:19 +0100

    @abstractmethod

--- priority_queue.py
+++ priority_queue.py
@@ -232,3 +232,29 @@ class PriorityQueue():
     @abstractmethod
     def decreasekey(self, node, new_priority): pass
 
+
+from abc import ABCMeta, abstractmethod
+import itertools
+
+from FibonacciHeap import FibHeap
+import heapq
+import Queue
+
+class PriorityQueue():
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def __len__(self): pass
+
+    @abstractmethod
+    def insert(self, node): pass
+
+    @abstractmethod
+    def minimum(self): pass
+
+    @abstractmethod
+    def removeminimum(self): pass
+
+    @abstractmethod
+    def decreasekey(self, node, new_priority): pass
+
