415989759275d14af362c1f3de7ed59a4532a143
PirateCat <106189370+pardonmycode@users.noreply.github.com>
2023-08-21 18:48:25 +0200

    finish version 1.2.1

--- profile.py
+++ profile.py
@@ -37,29 +37,3 @@ def profile():
 profiler = BProfile('profiler.png')
 with profiler:
     profile()
-#!/usr/bin/env python
-
-from bprofile import BProfile
-
-import tempfile
-from solve import solve
-from factory import SolverFactory
-
-methods = [
-    "astar",
-    # "breadthfirst",
-    # "depthfirst",
-    "dijkstra",
-    # "leftturn",
-]
-inputs = [
-    # "tiny",
-    # "small",
-    # "normal",
-    # "braid200",
-    "logo",
-    "combo400",
-    "braid2k",
-    "perfect2k",
-    # "perfect4k",
-    # "combo6k",
