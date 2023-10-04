9a3681c1fe3c76ecc38938b0bdf4357bf67a6a40
PirateCat <106189370+pardonmycode@users.noreply.github.com>
2023-10-04 05:34:24 +0200

    finish version 1.2.5

--- solve.py
+++ solve.py
@@ -88,3 +88,29 @@ def main():
 if __name__ == "__main__":
     main()
 
+from PIL import Image
+import time
+from mazes import Maze
+from factory import SolverFactory
+Image.MAX_IMAGE_PIXELS = None
+
+# Read command line arguments - the python argparse class is convenient here.
+import argparse
+
+def solve(factory, method, input_file, output_file):
+    # Load Image
+    print ("Loading Image")
+    im = Image.open(input_file)
+
+    # Create the maze (and time it) - for many mazes this is more time consuming than solving the maze
+    print ("Creating Maze")
+    t0 = time.time()
+    maze = Maze(im)
+    t1 = time.time()
+    print ("Node Count:", maze.count)
+    total = t1-t0
+    print ("Time elapsed:", total, "\n")
+
+    # Create and run solver
+    [title, solver] = factory.createsolver(method)
+    print ("Starting Solve:", title)
