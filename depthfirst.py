4f5fcfff289bc29cacd42a04f5a2a68ef9e7c63f
PirateCat <106189370+pardonmycode@users.noreply.github.com>
2023-10-09 04:56:58 +0200

    finish version 1.2.5

--- depthfirst.py
+++ depthfirst.py
@@ -90,3 +90,29 @@ def solve(maze):
         #import code
         #code.interact(local=locals())
 
+from collections import deque
+
+def solve(maze):
+    start = maze.start
+    end = maze.end
+    width = maze.width
+    stack = deque([start])
+    shape = (maze.height, maze.width)
+    prev = [None] * (maze.width * maze.height)
+    visited = [False] * (maze.width * maze.height)
+    count = 0
+
+    completed = False
+    while stack:
+        count += 1
+        current = stack.pop()
+
+        if current == end:
+            completed = True
+            break
+
+        visited[current.Position[0] * width + current.Position[1]] = True
+
+        #import code
+        #code.interact(local=locals())
+
